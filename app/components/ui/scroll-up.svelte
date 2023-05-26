b599c2606255707a493a3d4ebb8d2588cd2c7dcb
Vasanta Kumar <vasantakmr6@gmail.com>
2023-05-27 03:33:30 +0530

    dark mode

--- app/components/ui/scroll-up.svelte
+++ app/components/ui/scroll-up.svelte
@@ -25,7 +25,7 @@
 <style lang="scss">
 
     button {
-        @apply fixed bottom-5 right-5 text-gray4 bg-black bg-opacity-50 rounded-full w-10 h-10 outline-none 
+        @apply fixed bottom-5 right-5 text-gray4 bg-purple-300 bg-opacity-50 rounded-full w-10 h-10 outline-none 
                border-none opacity-0 translate-y-8 transition-all cursor-pointer p-0 grid place-items-center hover:text-orange-500;
         svg {
             @apply w-5 h-5;
